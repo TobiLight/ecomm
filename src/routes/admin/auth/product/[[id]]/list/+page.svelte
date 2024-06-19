7e81ba8ad39b432905dc332627f4b5e3ab9291a4
TobiLight <jgetitdonefast@gmail.com>
2024-06-19 21:53:38 +0100

    ...

--- src/routes/admin/auth/product/[[id]]/list/+page.svelte
+++ src/routes/admin/auth/product/[[id]]/list/+page.svelte
@@ -25,14 +25,11 @@
       link={name.singular}
     >
       <td class="px-6 py-4">
-        <span class="text-white">{item.categories && item.categories.length > 0 ? item.categories.map(category => category.name).join(", ") : "None"}</span>
-        <!-- {#each item.categories as category}
-          <span
-            class="bg-primary-100 text-primary-800 text-xs font-medium px-2 py-0.5 rounded dark:bg-primary-900 dark:text-primary-300"
-          >
-            {category.name}
-          </span>
-        {/each} -->
+        <span class="text-white"
+          >{item.categories && item.categories.length > 0
+            ? item.categories.map((category) => category.name).join(', ')
+            : 'None'}</span
+        >
       </td>
       <td class="px-6 py-4">
         <a href={item.image} target="_blank">
