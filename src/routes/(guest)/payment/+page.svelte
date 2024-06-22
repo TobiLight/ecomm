00f02cbe8a433c1d9e917c469038a60c5f34fb77
TobiLight <jgetitdonefast@gmail.com>
2024-06-22 13:11:53 +0100

    update

--- src/routes/(guest)/payment/+page.svelte
+++ src/routes/(guest)/payment/+page.svelte
@@ -8,14 +8,11 @@
 
   export let data;
 
-  const { form, errors, submitting, enhance } = superForm(data.form, {
-    validators: createOrderSchema,
-  });
+  let paymentMade: boolean = false
 
   $: cart = data.cart;
   $: products = data.products;
   $: total = products.reduce((total, product) => {
-    console.log(cart.get(product.id));
     return (total + product.price) * (cart.get(product.id) as number);
   }, 0);
 </script>
@@ -34,7 +31,7 @@
     <div class="w-full dark:text-white">
       <div class="grid gap-3">
         <p>Currently Accepted Payment Method:</p>
-        <ul class="ml-5">
+        <ul class="ml-5 grid gap-5">
           <div class="flex gap-2 items-center">
             <span>BTC Address:</span>
             <li
@@ -43,6 +40,27 @@
               dfghj3er6fyuik23456yuhbgvfdf
             </li>
           </div>
+          {#if paymentMad}
+            <form action="" class="grid gap-4">
+              <label
+                for="transactionHsh"
+                class="mb-2 block text-sm font-medium text-gray-900 dark:text-white"
+              >
+                Enter Transaction Hash
+                <input
+                  type="text"
+                  name="transactionHsh"
+                  placeholder="Enter the transaction hash for this payment"
+                  class="block w-96 mt-1 rounded-lg border p-2.5 text-sm focus:border-primary-600 focus:ring-primary-600 dark:focus:border-primary-500 dark:focus:ring-primary-500 border-gray-300 bg-gray-50 text-gray-900 dark:border-gray-600 dark:bg-gray-700 dark:text-white dark:placeholder-gray-400"
+                />
+              </label>
+
+              <button
+                class="bg-primary-600 hover:bg-primary-700 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 inline-flex items-center rounded-lg px-5 py-2.5 text-center text-sm font-medium text-white focus:ring-4 w-fit"
+                >Submit</button
+              >
+            </form>
+          {/if}
         </ul>
       </div>
     </div>
@@ -85,7 +103,7 @@
                 <div
                   class="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white"
                 >
-                  {currency}{product.price * (cart.get(product.id) || 1)}
+                  {currency}{product.price * Number(cart.get(product.id)) || 1}
                 </div>
               </div>
             </li>
@@ -106,6 +124,9 @@
               </div>
             </div>
             <button
+            on:click={e => {
+                paymentMade = true
+            }}
               type="button"
               class="mt-4 text-white bg-secondary-600 hover:bg-secondary-700 focus:ring-4 focus:outline-none focus:ring-secondary-200 dark:focus:ring-secondary-900 font-medium rounded-lg text-sm px-5 py-2.5 inline-flex justify-center w-full text-center"
               >I have made payment</button
